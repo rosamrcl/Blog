d2b4c4aea1129b0bc216e5c88e02b5365e50f020
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-03-25 15:02:36 -0300

    .

--- public/login.php
+++ public/login.php
@@ -3,7 +3,7 @@
                 $login=$_POST['login'];
                 $senha=$_POST['senha'];
 
-                header ('Location: ../public/comentario.php');
+                header ('Location:../public/comentario.php');
 
             }
             
